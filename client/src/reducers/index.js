b3f6edc19efa268a35b6b4d68cd5bc08d3da8cde
Mykol Benning <41842486+Mykolb@users.noreply.github.com>
2019-11-20 12:01:46 -0500

    Delete index.js

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { authenticationReducer } from './authenticationReducer';
-import { studentTableReducer } from './studentTableReducer';
-
-export const reducer = combineReducers({
-  authenticationReducer,
-  studentTableReducer
-});
-
